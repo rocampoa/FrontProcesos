998a303ffa2f7b84a6c8c25e46469bfa83cf3f71
rocampoa <rocampoa@gmail.com>
2020-03-23 21:18:09 -0500

    Se adiciona el menú

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,6 +6,7 @@ import {MatDialogModule} from '@angular/material/dialog';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatSelectModule} from '@angular/material/select';
+import {MatMenuModule} from '@angular/material/menu';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -14,7 +15,8 @@ const MaterialComponents = [
   MatDialogModule,
   MatInputModule,
   MatCardModule,
-  MatSelectModule
+  MatSelectModule,
+  MatMenuModule
 ];
 
 @NgModule({
